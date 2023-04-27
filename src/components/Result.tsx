d792ffea2a44cb2c93f8363d564e7d7e1e71b180
Jinson <web2feel@gmail.com>
2023-04-27 13:44:41 +0530

    fixed accessibility issues

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,13 +1,13 @@
 const Result = () => {
   return (
     <div className="bg-gradient-to-b from-[--light-slate-blue] to-[--light-royal-blue] rounded-bl-[32px] rounded-br-[32px] sm:rounded-[32px] text-center py-10 flex flex-col items-center">
-      <h2 className="text-2xl font-medium text-[--light-lavendar] -ml-[6px]">
+      <h1 className="text-2xl font-medium text-[--light-lavendar] -ml-[6px]">
         Your Result
-      </h2>
+      </h1>
       <div className="w-[200px] h-[200px] bg-gradient-to-b from-[--violet-blue] to-[--persian-blue] rounded-full mt-[30px] mb-[23px]">
-        <h1 className="text-[72px] leading-[72px] font-extrabold text-white mt-14">
+        <h2 className="text-[72px] leading-[72px] font-extrabold text-white mt-14">
           76
-        </h1>
+        </h2>
         <h3 className="text-[--light-lavendar] opacity-50 mt-1">of 100</h3>
       </div>
       <h2 className="text-[32px] text-white font-bold mt-1 -ml-1">Great</h2>
